04a5854e57cfd8090e31d9456f63f7317d655d0c
mateuszsliwa <maateuszsliwa7@wp.pl>
2026-01-13 22:53:12 +0100

    fix

--- src/utils/locale_utils.py
+++ src/utils/locale_utils.py
@@ -4,7 +4,7 @@ import os
 _LOCALES = {}
 
 def load_locale(lang):
-    path = os.path.join(os.environ.get("PROJECT_DIR"), "locales", f"{lang}.json")
+    path = os.path.join(os.environ.get("PROJECT_DIR"), "src", "locales", f"{lang}.json")
     with open(path, "r", encoding="utf-8") as f:
         _LOCALES[lang] = json.load(f)
 
